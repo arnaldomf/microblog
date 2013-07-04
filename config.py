3dedc8aa71e24ffbae3a1074d72b7f4164bbe9d2
arnaldomf <fakel@server.com>
2013-07-04 17:18:26 -0300

    Hello World Dealing with openids

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 CSRF_ENABLED = True
 SECRET_KEY = '\x8f\xab\xc4\xa00G\x17ZG\xd8\xb6\xe0\xb3\xe7\xf0\x8f\xa2\x98\xe5!\xc9\x0cB\xcb'
+OPENID_PROVIDERS = [
+	{ 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+	{ 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+	{ 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+	{ 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+	{ 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }
+]
