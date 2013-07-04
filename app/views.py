dae25c35c3b30c93556200e42cfac0446a32bf91
arnaldomf <fakel@server.com>
2013-07-04 15:03:30 -0300

    Hello World control statements

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'nickname': 'John'} # fake user
+	posts = [
+		{
+			'author': {'nickname': 'John'},
+			'body': 'Beatiful day in Portland'
+		},
+		{
+			'author': {'nickname': 'Susan'},
+			'body': 'The Avengers movie was so cool'
+		}
+	]
 	return render_template("index.html",
 		title='Home',
-		user = user)
+		user = user,
+		posts = posts)
